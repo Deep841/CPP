0748fdc8efbd4153f05e49cc776fb952fa24104d
Deep841 <kumardeep2003@gmail.com>
2026-06-15 00:53:32 +0530

    updation

--- 1-Sorting.cpp
+++ 1-Sorting.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-//Bubble Sort 
+//Bubble Sort - O(n^2), O(1)
 void bubbleSort(int arr[], int n){
     for (int i = 0; i < n-1; i++){
         bool isSwap = false;
@@ -18,7 +18,7 @@ void bubbleSort(int arr[], int n){
     }
 }
 
-//Selection Sort
+//Selection Sort - O(n^2), O(1)
 void SelectionSort(int arr[], int n){
     for(int i = 0; i<n-1; i++){
         int smallestIdx = i;
@@ -31,7 +31,7 @@ void SelectionSort(int arr[], int n){
     }
 }
 
-//Insertion Sort
+//Insertion Sort - O(n^2), O(1), O(n)=best
 void InsertionSort(int arr[],int n){
     for(int i=1; i<n; i++){
         int curr = arr[i];
@@ -45,4 +45,60 @@ void InsertionSort(int arr[],int n){
     }
 }
 
-//Merge Sort
+//Merge Sort - O(nlogn), O(n)
+void MergeSort(vector<int> &arr, int st, int e){
+    if(st<e){
+        int mid = st + (e - st)/2;
+        MergeSort(arr,st,mid);
+        MergeSort(arr,mid+1,e);
+
+        merge(arr,st,mid,e);
+    }
+}
+
+void merge(vector<int> &arr, int st, int mid, int e){
+    vector<int> temp;
+    int i = st, j = mid;
+
+    while(i<=mid && j<=e){
+        if(arr[i] > arr[j]) temp.push_back(arr[i++]);
+        else temp.push_back(arr[j++]);
+    }
+
+    while(i<=mid)   temp.push_back(arr[i++]);
+    while(j<=e)   temp.push_back(arr[j++]);
+
+    for(int idx=0; idx<temp.size(); idx++){
+        arr[idx+st] = temp[idx];
+    }
+}
+
+/*
+Quick Sort
+Best Case:      O(n log n)
+Average Case:   O(n log n)
+Worst Case:     O(n²)
+Space:          O(log n)    due to recursion stack
+*/
+void QuickSort(vector<int> &arr, int st,int e){
+    if(st<e){
+        int pivIdx = partition(arr, st, e);
+
+        QuickSort(arr, st, pivIdx-1);
+        QuickSort(arr, pivIdx+1,e);
+    }
+}
+
+int partition(vector<int> &arr, int st, int e){
+    int idx = st-1, pivot = arr[e];
+
+    for(int j=st; j<e; j++){
+        if(arr[j]<=pivot){
+            idx++;
+            swap(arr[j], arr[idx]);
+        }
+    }
+    idx++;
+    swap(arr[e],arr[idx]);
+    return idx;
+}
